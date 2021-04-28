1c0a895f722c79190ac2ab7ec5f1242c0af5aa16
brad@smithwebtek.com <brad@smithwebtek.com>
2021-04-28 11:36:02 -0400

    Setup provider, client, link, cach, .env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,26 @@ import './tailwind.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const link = createHttpLink({
+  uri: 'http://localhost:3000/graphql'
+})
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache()
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
